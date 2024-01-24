4ebe7ae31313e6e22d03f5c133c7cd238aab4e61
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-01-25 06:09:56 +0700

    onclite: Drop QTI camera device usage
    * qcom-camera topic hasn't been ported to 19.1
    * Keep building vendor.qti.hardware.camera.device@1.0 interface lib, IMS stack and possibly camera HAL still needs it
    
    Change-Id: I87bcd5b54ad986d5844df50de243fc1a18507198

--- device.mk
+++ device.mk
@@ -130,7 +130,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     libgui_vendor \
     libstdc++.vendor \
-    vendor.qti.hardware.camera.device@1.0
+    vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.device@1.0.vendor
 
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
