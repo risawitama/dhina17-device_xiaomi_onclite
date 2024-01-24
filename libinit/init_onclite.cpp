0f06899a7ec113e87a3e0ae6dfc42e386f7353d0
ItsVixano <giovanniricca@protonmail.com>
2024-01-24 16:47:58 +0000

    onclite: libinit: Import changes from sm8250-common
    * Omit everything related to fp/variant prop setting
    
    Change-Id: I96764491efc6b6e9614cc0fe870b2e3025d1a98d

--- libinit/init_onclite.cpp
+++ libinit/init_onclite.cpp
@@ -8,6 +8,8 @@
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
 
+#include <libinit_dalvik_heap.h>
+
 #include "vendor_init.h"
 
 // Define variants
@@ -60,4 +62,5 @@ void set_device_model() {
 
 void vendor_load_properties() {
     set_device_model();
+    set_dalvik_heap();
 }
