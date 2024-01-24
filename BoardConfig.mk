e175a8f67640a7d2435500229a7848fc441a594d
ItsVixano <giovanniricca@protonmail.com>
2024-01-25 05:42:03 +0700

    onclite: Move OVERRIDE_TARGET_FLATTEN_APEX and Add OVERRIDE_PRODUCT_COMPRESSED_APEX into device.mk
    Change-Id: I2dcf6b65d07e1f2ff14a48f6787690c109287e20

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,9 +16,6 @@
 
 DEVICE_PATH := device/xiaomi/onclite
 
-# APEX
-OVERRIDE_TARGET_FLATTEN_APEX := true
-
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
--- device.mk
+++ device.mk
@@ -16,6 +16,10 @@
 
 $(call inherit-product, vendor/xiaomi/onclite/onclite-vendor.mk)
 
+# APEX
+OVERRIDE_TARGET_FLATTEN_APEX := true
+OVERRIDE_PRODUCT_COMPRESSED_APEX := false
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
