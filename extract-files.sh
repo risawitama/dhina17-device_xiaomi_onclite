1fd3186fb280ab94ec5aee36a4ce0e3f0cc6e4c9
surenbaghdasaryan <surenb@google.com>
2024-02-14 15:50:25 +0000

    onclite: Replace writepid with task_profiles command for cgroup migration
    writepid command usage to join a cgroup has been deprecated in favor
    of a more flexible approach using task_profiles. This way cgroup path
    is not hardcoded and cgroup changes can be easily made. Replace
    writepid with task_profiles command to migrate between cgroups.
    
    Bug: 191283136
    Test: build and boot
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Change-Id: Ibb56d2379c42e5ed4b2f15c1b04d5fb163b48678

--- extract-files.sh
+++ extract-files.sh
@@ -58,6 +58,9 @@ function blob_fixup() {
     vendor/lib/hw/camera.msm8953.so)
         "${PATCHELF}" --add-needed libui_shim.so "${2}"
         ;;
+    vendor/etc/init/android.hardware.biometrics.fingerprint@2.1-service.rc)
+        sed -i 's|writepid /dev/cpuset/system-background/tasks|task_profiles ServiceCapacityLow|g' "${2}"
+        ;;
     esac
 }
 
